eb7120ab546c0a9348d0a8e705bf0665f12a1738
kjenkins5678 <kajenkins9@gmail.com>
2020-02-22 13:46:31 -0500

    select employee by department

--- index.js
+++ index.js
@@ -5,29 +5,11 @@ const cTable = require('console.table');
 var pool = mysql.createPool({
   connectionLimit: 10,
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "testtest",
   database: "employees_db"
 });
-
-var sqlViewAll = `SELECT
-  employees.id, 
-  employees.first_name, 
-  employees.last_name, 
-  roles.title,
-  departments.name as department,
-  roles.salary
-  FROM employees 
-  INNER JOIN roles ON employees.role_id = roles.id
-  INNER JOIN departments ON roles.department_id = departments.id`
-
 
 var directionsPrompt = {
   type: 'list',
@@ -44,31 +26,97 @@ var directionsPrompt = {
 ]
 };
 
-function DoPrompt() {
+function doPrompt() {
   inquirer.prompt(directionsPrompt).then(answers => {
     console.log(answers.direction);
 
     switch(answers.direction){
       case "View All Employees":
-        queryDB(sqlViewAll);
+        viewAll();
         break;
-      case "Quit":
+      case "View All Employees By Department":
+        queryDBbyDept();
         break;
     }
-  
   });
 }
 
-function queryDB(sql){
+function viewAll(){
+  var sqlViewAll = `SELECT
+  employees.id, 
+  employees.first_name, 
+  employees.last_name, 
+  roles.title,
+  departments.name as department,
+  roles.salary
+  FROM employees 
+  INNER JOIN roles ON employees.role_id = roles.id
+  INNER JOIN departments ON roles.department_id = departments.id`
   pool.getConnection(function (err, connection) {
     if (err) throw err;
-    connection.query(sql, function (err, rows) {
+    connection.query(sqlViewAll, function (err, rows) {
       connection.release();
       if (err) throw err;
-      console.table(rows);
-      DoPrompt();
+      console.table(rows)
+      doPrompt();
     });
   });
 }
 
-DoPrompt();
+function queryDBbyDept(){
+  var sqlSelectDept = `SELECT
+  name
+  FROM departments`
+  let depts = [];
+  pool.getConnection(function (err, connection) {
+    if (err) throw err;
+
+    connection.query(sqlSelectDept, function (err, rows) {
+      if (err) throw err;
+      for (i = 0; i<=rows.length-1; i++){
+        depts.push(rows[i]);
+      }
+      connection.release();
+      
+      inquirer
+        .prompt([{
+          /* Pass your questions in here */
+          type: 'list',
+          name: 'department',
+          message: 'Choose a Department',
+          choices: depts
+        }])
+        .then(answers => {
+          // Use user feedback for... whatever!!
+          console.log(answers.department)
+          var sqlByDept = `SELECT
+          employees.id, 
+          employees.first_name, 
+          employees.last_name, 
+          roles.title,
+          departments.name as department,
+          roles.salary
+          FROM employees 
+          INNER JOIN roles ON employees.role_id = roles.id
+          INNER JOIN departments ON roles.department_id = departments.id
+          WHERE departments.name = "${answers.department}"`
+          pool.getConnection(function (err, connection) {
+            if (err) throw err;
+            connection.query(sqlByDept, function (err, rows) {
+              connection.release();
+              if (err) throw err;
+              console.table(rows)
+              doPrompt();
+            });
+          });
+
+
+
+        });
+    });
+  });
+}
+
+
+
+doPrompt();
